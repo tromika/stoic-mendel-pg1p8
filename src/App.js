975c2afabefe2da6feef66010b45d82fe4fcaf57
Tamas Szuromi <tromika@gmail.com>
2021-08-30 16:20:54 +0200

    test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
       <h2>Start editing to see some magic happen!</h2>
       <h2>Start editing to see some magic happen!</h2>
       <h2>Start editing to see some magic happen!</h2>
+      <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
